ed9d713c8be18e70426fb623e04864a517b36b64
kogure-pyramid-inc-net <kogure@pyramid-inc.net>
2016-04-21 19:07:32 +0900

    rewrite版

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -7,3 +7,6 @@ yum -y install ntp httpd
 sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
 
+#設定ファイル挿入よりも.htccessに書いて設置した方がいいのかも
+#この書き方だとわかりにくすぎる。複数行挿入の何かいい書き方は。。
+sed -i -e "/^<Directory \"\/var\/www\/html\">$/a RewriteEngine On\nRewriteBase \/\nRewriteRule ^images\/(.+)$ assets/\$1 [L]\n" /etc/httpd/conf/httpd.conf
