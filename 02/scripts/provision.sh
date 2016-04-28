b2a3303d76f2ab0b764c3e565e40760e830ea05d
kogure-pyramid-inc-net <kogure@pyramid-inc.net>
2016-04-28 18:54:12 +0900

    課題02　nginxでリバースプロクシ

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -2,8 +2,11 @@
 set -eu
 
 yum -y update
-yum -y install ntp httpd
+rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+yum -y install ntp httpd nginx
 
-sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
-sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/Listen 80/Listen 8090/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8090/g" /etc/httpd/conf/httpd.conf
 
+sed -i -e "\$d" /etc/nginx/nginx.conf
+cat /vagrant/scripts/nginx.conf.append >> /etc/nginx/nginx.conf
