8bcaa3d660b745e3a5b4772a578466793eb10ff1
kogure-pyramid-inc-net <kogure@pyramid-inc.net>
2016-05-02 17:12:45 +0900

    basic認証

--- 03/scripts/provision.sh
+++ 03/scripts/provision.sh
@@ -7,3 +7,20 @@ yum -y install httpd
 sed -i -e "s/Listen 80$/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
 
+#単純に/var/www/htmlのAllowOverrideを書き換えて.htaccessを有効にすればいいのだが
+#残念なことにAllowOverride Noneはいろいろな場所にある。
+
+#最下行に同じ<Directory "/var/www/html">設定を追記すると動くは動くけど、書いた以外の設定は残るのか破棄されるのか不明。。。
+#printf "<Directory \"/var/www/html\">\nAllowOverride All\n</Directory>" >> /etc/httpd/conf/httpd.conf
+
+#しかしながら行番号指定置換はちょっと。。。
+sed -i -e "338s/AllowOverride None/AllowOverride All/g" /etc/httpd/conf/httpd.conf
+
+#かといって2番目出現指定も危ない
+#sed -i -e "s/AllowOverride None/AllowOverride All/2" /etc/httpd/conf/httpd.conf
+
+if [ $# == 2 ]; then
+	echo $@ #特に意味はない
+    cp /vagrant/scripts/.htaccess /var/www/html/
+    htpasswd -bc /var/www/.htpasswd $1 $2
+fi
