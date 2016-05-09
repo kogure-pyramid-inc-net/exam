cba9bb9cfbbbc5ec02a632d95d9059bd386607d7
kogure-pyramid-inc-net <kogure@pyramid-inc.net>
2016-05-09 15:29:59 +0900

    設定追記方法をnginx.confに直書きからconf.dに置くように変更。 再provisionのときにrpmで止まらないようにした。

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -2,11 +2,22 @@
 set -eu
 
 yum -y update
-rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+#-Uにしてもコケる(多分rpmのalready installedがエラー扱いになってるのでは？)ので、
+#パッケージ存在で処理分けるしか無い。というか単にyumにあればそれで済むのに！
+if rpm -qa | grep nginx > /dev/null
+then
+	echo "nginx packege already exists."
+else
+	rpm -Uvh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+fi
 yum -y install ntp httpd nginx
 
-sed -i -e "s/Listen 80/Listen 8090/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/Listen 80$/Listen 8090/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8090/g" /etc/httpd/conf/httpd.conf
 
-sed -i -e "\$d" /etc/nginx/nginx.conf
-cat /vagrant/scripts/nginx.conf.append >> /etc/nginx/nginx.conf
+if [ -e /etc/nginx/conf.d/reverse_proxy.conf ]; then
+	echo "/etc/nginx/conf.d/reverse_proxy.conf already exists. not copied."
+else
+	#nginx.confの中に　include /etc/nginx/conf.d/*.conf;　が書いてあるので追加設定を.confでコピーしとけば読み込まれる。
+	cp /vagrant/scripts/reverse_proxy.conf /etc/nginx/conf.d/
+fi
