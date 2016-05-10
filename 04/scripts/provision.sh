b028469c1ab698110caf84be826d9c8b6fd98153
kogure-pyramid-inc-net <kogure@pyramid-inc.net>
2016-05-10 16:27:48 +0900

    fluentdローカルマシンに吐く設定

--- 04/scripts/provision.sh
+++ 04/scripts/provision.sh
@@ -7,3 +7,17 @@ yum -y install httpd
 sed -i -e "s/Listen 80$/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
 
+chmod -R a+x /var/log/httpd
+
+#install fluentd
+if [ -e /etc/td-agent/conf.d/httpd_log.conf ]; then
+	echo "/etc/td-agent/conf.d/httpd_log.conf already exists."
+else
+	#インストール途中でコケたときのことを考えると１行づつチェックすべきか。。。
+	curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | sh
+	chmod -R a+w /vagrant/app/logs
+	chmod -R a+w /var/log/td-agent/
+	mkdir /etc/td-agent/conf.d/
+	cp /vagrant/scripts/httpd_log.conf /etc/td-agent/conf.d/
+	echo "@include conf.d/*.conf" >> /etc/td-agent/td-agent.conf
+fi
--- 04/scripts/run.sh
+++ 04/scripts/run.sh
@@ -3,7 +3,8 @@ set -eu
 
 function is_running() {
 	SERVICE_NAME=$1
-	IS_SERVICE_RUNNING=$(service $SERVICE_NAME status | grep "running")
+	#td-agentは上がってないときstatusがis not runnningになるのでrunnningが含まれてしまう。
+	IS_SERVICE_RUNNING=$(service $SERVICE_NAME status | grep "is running")
 	if [ "$IS_SERVICE_RUNNING" == "" ]; then
 		echo 'false'
 	else
@@ -16,4 +17,11 @@ if [ "$IS_HTTPD_RUNNING" == 'true' ]; then
   service httpd restart
 else
   service httpd start
+fi
+
+IS_FLUENTD_RUNNING=$(is_running td-agent)
+if [ "$IS_FLUENTD_RUNNING" == 'true' ]; then
+  service td-agent restart
+else
+  service td-agent start
 fi
